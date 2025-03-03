cbba535e51bbc0d606aa688f643d147fca68cf06
Joel Tannas <jtannas@gmail.com>
2025-03-03 15:25:42 -0700

    `bundle gem katachi`

---
+++ spec/katachi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Katachi do
+  it "has a version number" do
+    expect(Katachi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
