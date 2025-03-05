58613e898cd95d94497ccba176ab23828392dc06
Joel Tannas <jtannas@gmail.com>
2025-03-05 16:32:54 -0700

    validate numbers; learn that `===` is awesome

--- lib/katachi/validator.rb
+++ lib/katachi/validator.rb
@@ -7,17 +7,16 @@ class Katachi::Validator
   def self.validate(value:, shapes:)
     case value
     when String then validate_string(value:, shapes:)
+    when Number then validate_number(value:, shapes:)
     else raise NotImplementedError
     end
   end
 
   def self.valid_string?(value:, shapes:)
-    shapes.any? do |shape|
-      case shape
-      when DIRECTIVE_REGEX then false
-      when String, Regexp then shape.match?(value)
-      else false # rubocop:disable Lint/DuplicateBranch
-      end
-    end
+    shapes.any? { |shape| !(DIRECTIVE_REGEX === shape) && shape === value }
+  end
+
+  def self.valid_number?(value:, shapes:)
+    shapes.any? { |shape| !(DIRECTIVE_REGEX === shape) && shape === value }
   end
 end
--- spec/katachi/validator_spec.rb
+++ spec/katachi/validator_spec.rb
@@ -10,7 +10,25 @@ RSpec.describe Katachi::Validator do
         { value: "foo", shapes: [/foo/] } => true,
         { value: "foo", shapes: [:foo] } => false,
         { value: "foo", shapes: ["$foo:bar"] } => false,
-        { value: "foo", shapes: ["$foo:bar"] } => false,
+        { value: "c", shapes: ["c"..."x"] } => true,
+        { value: "c", shapes: ["d"..."x"] } => false,
+        { value: "c", shapes: [1...3] } => false,
+      }
+    )
+  end
+
+  describe ".valid_number?" do
+    it_behaves_like(
+      "a pure kwargs function",
+      method: :valid_number?,
+      kwargs_to_outputs: {
+        { value: 1, shapes: [1] } => true,
+        { value: 1, shapes: [1...2] } => true,
+        { value: 1, shapes: [:foo] } => false,
+        { value: 1, shapes: ["$foo:bar"] } => false,
+        { value: 1, shapes: [1...4] } => true,
+        { value: 1, shapes: [4...7] } => false,
+        { value: 1, shapes: ["a"..."d"] } => false,
       }
     )
   end
