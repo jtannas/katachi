507316936a4f1b4f7b08de52ca8ebc6d957e5fe1
Joel Tannas <jtannas@gmail.com>
2025-03-07 17:03:57 -0700

    WIP: retooling validation to provide feedback to user

--- lib/katachi/validator.rb
+++ lib/katachi/validator.rb
@@ -5,35 +5,59 @@ class Katachi::Validator
   DIRECTIVE_REGEX = /^\$\w*:.*$/
   EXTRA_KEYS_FLAG = "$extra_keys"
 
-  def self.valid?(value:, shapes:)
+  def self.validate(value:, shapes:)
+    messages = []
+    value_text = case value
+                 when String then "\"#{value}\""
+                 when Symbol then ":#{value}"
+                 else value.inspect
+                 end
     valid_shapes = shapes.reject { |s| DIRECTIVE_REGEX === s }
-    valid_shapes.any? do |shape|
+    valid_shapes.each do |shape|
+      shape_text = case shape
+                   when String then "\"#{shape}\""
+                   when Symbol then ":#{shape}"
+                   else shape.inspect
+                   end
+      pass_message = "=> PASS: Value `#{value_text}` matched shape `#{shape_text}`"
+      fail_message = "=> FAIL: Value `#{value_text}` does not match shape `#{shape_text}`"
       case value
-      when Array then valid_array?(array: value, shape:)
-      when Hash then valid_hash?(hash: value, shape:)
-      else shape === value # rubocop:disable Style/CaseEquality
+      when Array then messages.concat(validate_array(hash: value, shape:))
+      when Hash then messages.concat(validate_hash(hash: value, shape:))
+      else messages << (shape === value ? pass_message : fail_message)
       end
     end
+    has_pass = messages.any? { |m| m.start_with?("=> PASS") }
+    header = if has_pass
+               "PASS: Value `#{value_text}` matched a shape"
+             else
+               "FAIL: Value `#{value_text}` does not match any of the shapes"
+             end
+    [header, *messages].join("\n")
   end
 
-  def self.valid_hash?(hash:, shape:)
-    return true if shape == Hash
-    return false unless shape.is_a?(Hash)
-    return false unless shape.delete(EXTRA_KEYS_FLAG) || (hash.keys - shape.keys).empty?
+  def self.validate_hash(hash:, shape:)
+    return [] if shape == Hash
+    return ["is_not_same_class as #{shape}"] unless shape.is_a?(Hash)
+
+    unless shape.delete(EXTRA_KEYS_FLAG)
+      extra_keys = (hash.keys - shape.keys)
+      return ["has extra_keys: [#{extra_keys.join(",")}]"] if extra_keys.any?
+    end
 
     shape.all? do |k, sub_shapes|
       if hash.key?(k)
-        valid?(value: hash[k], shapes: sub_shapes)
+        validate(value: hash[k], shapes: sub_shapes)
       else
         sub_shapes.include?(:undefined)
       end
     end
   end
 
-  def self.valid_array?(array:, shape:)
-    return true if shape == Array
-    return false unless shape.is_a?(Array)
+  def self.validate_array(array:, shape:, messages:)
+    return [] if shape == Array
+    return ["is_not_same_class as #{shape}"] unless shape.is_a?(Array)
 
-    array.all? { |element| valid?(value: element, shapes: shape) }
+    array.map { |element| "=> " + validate(value: element, shapes: shape) }
   end
 end
--- spec/katachi/validator_spec.rb
+++ spec/katachi/validator_spec.rb
@@ -1,91 +1,164 @@
 # frozen_string_literal: true
 
 RSpec.describe Katachi::Validator do
-  describe ".valid?" do
+  describe ".validate" do
     it_behaves_like(
       "a pure kwargs function",
-      method: :valid?,
+      method: :validate,
       kwargs_to_outputs: {
         # String
-        { value: "foo", shapes: ["foo"] } => true,
-        { value: "foo", shapes: [/foo/] } => true,
-        { value: "foo", shapes: [:foo] } => false,
-        { value: "foo", shapes: ["$foo:bar"] } => false,
-        { value: "c", shapes: ["c"..."x"] } => true,
-        { value: "c", shapes: ["d"..."x"] } => false,
-        { value: "c", shapes: [1...3] } => false,
+        { value: "foo", shapes: ["foo"] } => <<~RETURN.chomp,
+          PASS: Value `"foo"` matched a shape
+          => PASS: Value `"foo"` matched shape `"foo"`
+        RETURN
+        { value: "foo", shapes: [/foo/] } => <<~RETURN.chomp,
+          PASS: Value `"foo"` matched a shape
+          => PASS: Value `"foo"` matched shape `/foo/`
+        RETURN
+        { value: "foo", shapes: [:foo] } => <<~RETURN.chomp,
+          FAIL: Value `"foo"` does not match any of the shapes
+          => FAIL: Value `"foo"` does not match shape `:foo`
+        RETURN
+        { value: "foo", shapes: ["$foo:bar", /bar/] } => <<~RETURN.chomp,
+          FAIL: Value `"foo"` does not match any of the shapes
+          => FAIL: Value `"foo"` does not match shape `/bar/`
+        RETURN
+        { value: "c", shapes: ["c"..."x"] } => <<~RETURN.chomp,
+          PASS: Value `"c"` matched a shape
+          => PASS: Value `"c"` matched shape `"c"..."x"`
+        RETURN
+        { value: "c", shapes: ["d"..."x"] } => <<~RETURN.chomp,
+          FAIL: Value `"c"` does not match any of the shapes
+          => FAIL: Value `"c"` does not match shape `"d"..."x"`
+        RETURN
+        { value: "c", shapes: [1...3, "c", "foo"] } => <<~RETURN.chomp,
+          PASS: Value `"c"` matched a shape
+          => FAIL: Value `"c"` does not match shape `1...3`
+          => PASS: Value `"c"` matched shape `"c"`
+          => FAIL: Value `"c"` does not match shape `"foo"`
+        RETURN
+        { value: "c", shapes: [1...3, :foo, "foo"] } => <<~RETURN.chomp,
+          FAIL: Value `"c"` does not match any of the shapes
+          => FAIL: Value `"c"` does not match shape `1...3`
+          => FAIL: Value `"c"` does not match shape `:foo`
+          => FAIL: Value `"c"` does not match shape `"foo"`
+        RETURN
         # Numbers
-        { value: 1, shapes: [1] } => true,
-        { value: 1, shapes: [1...2] } => true,
-        { value: 1, shapes: [:foo] } => false,
-        { value: 1, shapes: ["$foo:bar"] } => false,
-        { value: 1, shapes: [1...4] } => true,
-        { value: 1, shapes: [4...7] } => false,
-        { value: 1, shapes: ["a"..."d"] } => false,
+        { value: 1, shapes: [1] } => <<~RETURN.chomp,
+          PASS: Value `1` matched a shape
+          => PASS: Value `1` matched shape `1`
+        RETURN
+        { value: 1, shapes: [1...2] } => <<~RETURN.chomp,
+          PASS: Value `1` matched a shape
+          => PASS: Value `1` matched shape `1...2`
+        RETURN
+        { value: 1, shapes: [:foo] } => <<~RETURN.chomp,
+          FAIL: Value `1` does not match any of the shapes
+          => FAIL: Value `1` does not match shape `:foo`
+        RETURN
+        { value: 1, shapes: ["$foo:bar"] } => <<~RETURN.chomp,
+          FAIL: Value `1` does not match any of the shapes
+        RETURN
+        { value: 1, shapes: [4...7] } => <<~RETURN.chomp,
+          FAIL: Value `1` does not match any of the shapes
+          => FAIL: Value `1` does not match shape `4...7`
+        RETURN
+        { value: 1, shapes: ["a"..."d"] } => <<~RETURN.chomp,
+          FAIL: Value `1` does not match any of the shapes
+          => FAIL: Value `1` does not match shape `"a"..."d"`
+        RETURN
         # Booleans
-        { value: true, shapes: [true] } => true,
-        { value: false, shapes: [false] } => true,
-        { value: true, shapes: [false] } => false,
-        { value: false, shapes: [true] } => false,
-        { value: true, shapes: [1] } => false,
-        { value: true, shapes: [:foo] } => false,
-        { value: true, shapes: ["$foo:bar"] } => false,
+        { value: true, shapes: [true] } => <<~RETURN.chomp,
+          PASS: Value `true` matched a shape
+          => PASS: Value `true` matched shape `true`
+        RETURN
+        { value: false, shapes: [false] } => <<~RETURN.chomp,
+          PASS: Value `false` matched a shape
+          => PASS: Value `false` matched shape `false`
+        RETURN
+        { value: true, shapes: [false] } => <<~RETURN.chomp,
+          FAIL: Value `true` does not match any of the shapes
+          => FAIL: Value `true` does not match shape `false`
+        RETURN
+        { value: false, shapes: [true] } => <<~RETURN.chomp,
+          FAIL: Value `false` does not match any of the shapes
+          => FAIL: Value `false` does not match shape `true`
+        RETURN
+        { value: true, shapes: [1] } => <<~RETURN.chomp,
+          FAIL: Value `true` does not match any of the shapes
+          => FAIL: Value `true` does not match shape `1`
+        RETURN
+        { value: true, shapes: [:foo] } => <<~RETURN.chomp,
+          FAIL: Value `true` does not match any of the shapes
+          => FAIL: Value `true` does not match shape `:foo`
+        RETURN
+        { value: true, shapes: ["$foo:bar"] } => <<~RETURN.chomp,
+          FAIL: Value `true` does not match any of the shapes
+        RETURN
         # Nil
-        { value: nil, shapes: [nil] } => true,
-        { value: nil, shapes: [1] } => false,
-        { value: nil, shapes: [] } => false,
-        # Simple Arrays
-        { value: [], shapes: [[]] } => true,
-        { value: [], shapes: [Array] } => true,
-        { value: [1], shapes: [Array] } => true,
-        { value: [], shapes: [[Integer]] } => true,
-        { value: [1], shapes: [[Integer]] } => true,
-        { value: [1, 2, 3], shapes: [[Integer]] } => true,
-        { value: [1, 2, 3, "a"], shapes: [[Integer]] } => false,
-        { value: [1, "a"], shapes: [[Integer, String]] } => true,
-        { value: [nil], shapes: [[Integer, nil]] } => true,
-        # Nested Arrays
-        { value: [[nil]], shapes: [[Integer, nil]] } => false,
-        { value: [[nil]], shapes: [[[nil]]] } => true,
-        { value: [1, 2, ["a"]], shapes: [[Integer, [String]]] } => true,
-        { value: [
-            %w[First Last Age],
-            ["John", "Doe", 42],
-            ["Jane", "Doris", 59]
-          ],
-          shapes: [[[String, Integer]]] } => true,
-        # Hashes
-        { value: { a: 1 }, shapes: [Hash] } => true,
-        { value: { a: {} }, shapes: [{ a: [Hash] }] } => true,
-        { value: { a: nil }, shapes: [{ a: [Integer, nil] }] } => true,
-        {
-          value: { first: "John", last: "Doe", dob: Time.now },
-          shapes: [{ first: [String], last: [String], dob: [Time] }],
-        } => true,
-        {
-          value: { first: "John", last: "Doe", age: 42 },
-          shapes: [{ first: [String], last: [String], dob: [Time] }],
-        } => false,
-        {
-          value: {
-            first: "John",
-            last: "Doe",
-            dob: Time.now,
-            spouse: { first: "Jane", last: "Doe", dob: Time.now },
-          },
-          shapes: [{
-            first: [String],
-            last: [String],
-            dob: [Time],
-            spouse: [nil, { first: [String], last: [String], dob: [Time] }],
-          }],
-        } => true,
-        # Hashes with missing keys
-        { value: { a: {} }, shapes: [{ a: [Hash], b: [Integer] }] } => false,
-        { value: { a: 1 }, shapes: [{ a: [Integer], b: [Integer, :undefined] }] } => true,
-        # Hashes with extra keys
-        { value: { a: 1, b: 2 }, shapes: [{ a: [Integer] }] } => false,
-        { value: { a: 1, b: 2 }, shapes: [{ a: [Integer], "$extra_keys" => true }] } => true,
+        { value: nil, shapes: [nil] } => <<~RETURN.chomp,
+          PASS: Value `nil` matched a shape
+          => PASS: Value `nil` matched shape `nil`
+        RETURN
+        { value: nil, shapes: [1] } => <<~RETURN.chomp,
+          FAIL: Value `nil` does not match any of the shapes
+          => FAIL: Value `nil` does not match shape `1`
+        RETURN
+        { value: nil, shapes: [] } => <<~RETURN.chomp,
+          FAIL: Value `nil` does not match any of the shapes
+        RETURN
+        # # Simple Arrays
+        # { value: [], shapes: [[]] } => [],
+        # { value: [], shapes: [Array] } => [],
+        # { value: [1], shapes: [Array] } => [],
+        # { value: [], shapes: [[Integer]] } => [],
+        # { value: [1], shapes: [[Integer]] } => [],
+        # { value: [1, 2, 3], shapes: [[Integer]] } => [],
+        # { value: [1, 2, 3, "a"], shapes: [[Integer]] } => false,
+        # { value: [1, "a"], shapes: [[Integer, String]] } => [],
+        # { value: [nil], shapes: [[Integer, nil]] } => [],
+        # # Nested Arrays
+        # { value: [[nil]], shapes: [[Integer, nil]] } => false,
+        # { value: [[nil]], shapes: [[[nil]]] } => [],
+        # { value: [1, 2, ["a"]], shapes: [[Integer, [String]]] } => [],
+        # { value: [
+        #     %w[First Last Age],
+        #     ["John", "Doe", 42],
+        #     ["Jane", "Doris", 59]
+        #   ],
+        #   shapes: [[[String, Integer]]] } => [],
+        # # Hashes
+        # { value: { a: 1 }, shapes: [Hash] } => [],
+        # { value: { a: {} }, shapes: [{ a: [Hash] }] } => [],
+        # { value: { a: nil }, shapes: [{ a: [Integer, nil] }] } => [],
+        # {
+        #   value: { first: "John", last: "Doe", dob: Time.now },
+        #   shapes: [{ first: [String], last: [String], dob: [Time] }],
+        # } => [],
+        # {
+        #   value: { first: "John", last: "Doe", age: 42 },
+        #   shapes: [{ first: [String], last: [String], dob: [Time] }],
+        # } => false,
+        # {
+        #   value: {
+        #     first: "John",
+        #     last: "Doe",
+        #     dob: Time.now,
+        #     spouse: { first: "Jane", last: "Doe", dob: Time.now },
+        #   },
+        #   shapes: [{
+        #     first: [String],
+        #     last: [String],
+        #     dob: [Time],
+        #     spouse: [nil, { first: [String], last: [String], dob: [Time] }],
+        #   }],
+        # } => [],
+        # # Hashes with missing keys
+        # { value: { a: {} }, shapes: [{ a: [Hash], b: [Integer] }] } => false,
+        # { value: { a: 1 }, shapes: [{ a: [Integer], b: [Integer, :undefined] }] } => [],
+        # # Hashes with extra keys
+        # { value: { a: 1, b: 2 }, shapes: [{ a: [Integer] }] } => false,
+        # { value: { a: 1, b: 2 }, shapes: [{ a: [Integer], "$extra_keys" => true }] } => [],
       }
     )
   end
