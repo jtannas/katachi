f8a128904a105f1117c7abce8764c153d11b3a4f
Joel Tannas <jtannas@gmail.com>
2025-03-13 22:07:13 -0600

    build usage example

--- lib/katachi.rb
+++ lib/katachi.rb
@@ -8,4 +8,10 @@ require_relative "katachi/validator"
 require_relative "katachi/any_of"
 
 # A tool for describing objects in a compact and readable way
-module Katachi; end
+module Katachi
+  def validate(**) = Validator.validate(**)
+  module_function :validate
+
+  def any_of(*shapes) = AnyOf.new(*shapes)
+  module_function :any_of
+end
--- spec/katachi_spec.rb
+++ spec/katachi_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Katachi do
   it "has a version number" do
     expect(Katachi::VERSION).not_to be_nil
   end
+
+  Kt = Katachi
+  it "validates shapes" do
+    value = { a: { b: [1, "a"] } }
+    shape = { a: { b: [Kt.any_of(Integer, String)] } }
+    expect(Kt.validate(value:, shape:)).to be_match
+  end
 end
