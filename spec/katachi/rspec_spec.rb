8f83d05ffa108fc199efbc71fc43eb7c24be6ba8
Joel Tannas <jtannas@gmail.com>
2025-03-14 18:09:27 -0600

    appease rubocop

--- spec/katachi/rspec_spec.rb
+++ spec/katachi/rspec_spec.rb
@@ -2,14 +2,14 @@
 
 require "katachi/rspec"
 
-RSpec.describe "Custom RSpec Matchers" do
+RSpec.describe "Custom RSpec Matchers" do # rubocop:disable RSpec/DescribeClass
   describe "have_shape" do
     it "passes with compatible shapes" do
       value = "hello_world"
       expect(value).to have_shape(String)
     end
 
-    it "fails with incompatible shapes" do
+    it "fails with incompatible shapes" do # rubocop:disable RSpec/MultipleExpectations
       value = "hello_world"
 
       expect { expect(value).to have_shape(Integer) }
