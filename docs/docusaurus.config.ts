92bec6d97f65dfc1577c19dcd1de3fbe3e929db6
Joel Tannas <jtannas@gmail.com>
2025-03-19 23:15:00 -0600

    add analytics to docs website (#56)
    * add analytics to docs website
    
    * add test workflow
    
    * fix dir on workflow

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -93,6 +93,12 @@ tagline: 'A Ruby tool for describing objects as intuitively as possible',
       darkTheme: prismThemes.vsDark,
       additionalLanguages: ['ruby', 'diff'],
     },
+    plugins: [
+      [
+        "posthog-docusaurus",
+        { apiKey: "phc_CNDu1lfT2NF1WKrKWNMIaMiWiYzd7cAX7XQPLIYkM1r" },
+      ],
+    ],
   } satisfies Preset.ThemeConfig,
 };
 
